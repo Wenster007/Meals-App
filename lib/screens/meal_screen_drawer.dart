bada41749091dd74c1d16dfce326aa7dffcd935e
Ahmed Khan <106860847+Wenster007@users.noreply.github.com>
2023-06-07 21:49:55 +0500

    added side Drawers to the mainScreen

---
+++ lib/screens/meal_screen_drawer.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class MealScreenDrawer extends StatelessWidget {
+  const MealScreenDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          DrawerHeader(
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+                Theme.of(context).colorScheme.primaryContainer,
+                Theme.of(context).colorScheme.primaryContainer.withOpacity(0.8),
+              ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+            ),
+            child: Row(
+              children: [
+                Icon(
+                  Icons.fastfood,
+                  size: 28,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+                const SizedBox(
+                  width: 18,
+                ),
+                Text(
+                  "Cooking Up!",
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleLarge!
+                      .copyWith(color: Theme.of(context).colorScheme.primary),
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.restaurant,
+              color: Theme.of(context).colorScheme.onBackground,
+              size: 26,
+            ),
+            title: Text(
+              "Meals",
+              style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                    fontSize: 24,
+                  ),
+            ),
+            onTap: () {},
+          )
+        ],
+      ),
+    );
+  }
+}
